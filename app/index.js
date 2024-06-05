e1b67a9b7cfae32eac5f70d6c2d547e450cc7835
breebonner <bab3pz@virginia.edu>
2024-06-04 23:45:20 -0400

    index

--- app/index.js
+++ app/index.js
@@ -1,28 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'expo-router';
 import useAuth from '../hooks/useAuth';
+import IntroScreen from '../app/IntroScreen'
 
 const Index = () => {
   const router = useRouter();
   const { user } = useAuth();
   const [isReady, setReady] = useState(false);
+  const [showIntro, setShowIntro] = useState(true);
 
   useEffect(() => {
-    setReady(true);
+
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+      setReady(true);
+    }, 3000); // Display intro screen for 3 seconds
+
+    return () => clearTimeout(timer); // Clear timer on unmount
   }, []);
 
   useEffect(() => {
-    if (isReady) {
+    if (isReady && !showIntro) {
       if(user) {
         router.replace('/HomeScreen');
-      }
-
-      else {
+      } else {
         router.replace('/WelcomeScreen');
       }
-      
+
     }
-  }, [isReady]);
+  }, [isReady, showIntro]);
+
+  if (showIntro) {
+    return <IntroScreen />
+  }
 
   return null; 
 };
